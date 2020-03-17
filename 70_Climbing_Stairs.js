ea1483ca58113325c39a7b73717244fc1ea4153e
Marcos Barrera <elyomarcos@gmail.com>
2020-03-16 23:15:57 -0600

    Easy - 70. Climbing Stairs - Recursive WITH memoization enhancement

--- 70_Climbing_Stairs.js
+++ 70_Climbing_Stairs.js
@@ -1,15 +1,17 @@
 /**
  * @param {number} n
  * @return {number}
+ * Roughly O(N) with memoization
  */
-const climbStairs = (n) => {
+const climbStairs = (n, savedResults = {}) => {
+  if(n in savedResults) return savedResults[n];
   if (n < 0) return 0;
   if(n === 0) return 1;
-
-  return climbStairs(n - 2) + climbStairs(n - 1);
+  savedResults[n] = climbStairs(n - 2, savedResults) + climbStairs(n - 1, savedResults);
+  return savedResults[n];
 };
 
 // debugger;
-const result = climbStairs(40);
+const result = climbStairs(5);
 console.log('result: ', result);
 // debugger;
