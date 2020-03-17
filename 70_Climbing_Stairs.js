9c04149f98025ed3fa5a673033357e96585641aa
Marcos Barrera <elyomarcos@gmail.com>
2020-03-16 23:43:05 -0600

    Easy - 70. Climbing Stairs - Bottom Up DP - O(N) Time  - O(1) Space

--- 70_Climbing_Stairs.js
+++ 70_Climbing_Stairs.js
@@ -17,15 +17,15 @@ const climbStairsRecursive = (n, savedResults = {}) => {
  * @return {number}
  * O(N) Space and Time
  */
-const climbStairsBottomUp = (n) => {
+const climbStairsBottomUpNoSpaceOverhead = (n) => {
   if (n < 0) return 0;
   if(n >= 0 && n < 2) return 1;
 
-  const array = [1, 1];
-  while(array.length < n + 1) array.push(undefined);
+  const array = [1, 1, undefined];
 
   for(let i = 2; i < n + 1; i++) {
-    array[i] = array[i - 2] + array[i - 1];
+    array[2] = array[0] + array[1];
+    array.shift();
   }
 
   return array[array.length - 1];
@@ -33,6 +33,6 @@ const climbStairsBottomUp = (n) => {
 
 // debugger;
 // const result = climbStairsRecursive(5);
-const result = climbStairsBottomUp(6);
+const result = climbStairsBottomUpNoSpaceOverhead(6);
 console.log('result: ', result);
 // debugger;
